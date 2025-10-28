01abfecfc653fe471935cca8fe2bc3718854391a
tsal4 <159286965+tsal4@users.noreply.github.com>
2025-10-28 13:54:48 -0400

    added speech-to-text to root.py

--- speech_to_text_model/stt_model.py
+++ speech_to_text_model/stt_model.py
@@ -3,35 +3,22 @@ import whisper
 model = whisper.load_model("tiny")
 
 # normal .wav file
-#audio_path = r"C:\Users\tadsa\GitHub\capstone_project\speech_to_text_model\voice-sample.wav"
+audio_path = r"C:\Users\tadsa\GitHub\capstone_project\speech_to_text_model\voice-sample.wav"
 
-# normal .m4a file
-#audio_path = r"C:\Users\tadsa\aaaaa_test_audio\test_4_normal_audio.m4a"
+def speech_to_text(audio_file):
+    audio = whisper.load_audio(audio_file)
+    audio = whisper.pad_or_trim(audio)
 
-# far away audio
-#audio_path = r"C:\Users\tadsa\aaaaa_test_audio\test_2_far_away.m4a"
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
 
-# sound interfernce
-#audio_path = r"C:\Users\tadsa\aaaaa_test_audio\test_3_interference.m4a"
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    #print(f"Detected language: {max(probs, key=probs.get)}")
 
-# accent (scottish)
-audio_path = r"C:\Users\tadsa\aaaaa_test_audio\test_5_accent.m4a"
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
 
-
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio(audio_path)
-audio = whisper.pad_or_trim(audio)
-
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
-
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
-
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
-
-# print the recognized text
-print(result.text)
+    # return the recognized text
+    return(result)
