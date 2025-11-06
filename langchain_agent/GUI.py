e1ad773ccfda816c69fd6193301a89f156c02dbd
steveloya19 <sloya25@jcu.edu>
2025-11-06 12:18:30 -0500

    Update GUI.py

--- langchain_agent/GUI.py
+++ langchain_agent/GUI.py
@@ -25,59 +25,71 @@ class Alfred(App):
         
         #Label Widget
         self.greeting = Label(
-                            text = "Welcome, ask a question below!",
+                            text = "Alfred: Welcome, ask a question!",
                             font_size = 25,
                             color ="white"
                              )
         self.window.add_widget(self.greeting)
-        #User input widget
-        self.user = TextInput(
 
-                        padding_y = (20,20),
-                        size_hint = (1,0.5)
-        )
 
-        self.window.add_widget(self.user)
-                
-        #Button Widget
-        self.button = Button(text = "Enter",
-                             size_hint = (1,0.5), 
-                             bold = True,
-                             background_color = "#0099FF",
-                             background_normal = ""
-                             
+        self.question = Label(
+                            text = "You: This is where the User will have their response",
+                            font_size = 25,
+                            color ="white"
                              )
+        
+        self.window.add_widget(self.question)
+ 
+        #User input widget
+        #self.user = TextInput(
+
+            #            padding_y = (20,20),
+           #             size_hint = (1,0.5)
+       # )
+
+       # self.window.add_widget(self.user)
+
+        
+        #Button Widget
+       # self.button = Button(text = "Mic",
+                           #  size_hint = (1,0.5), 
+                          #   bold = True,
+                           #  background_color = "#0099FF",
+                           #  background_normal = ""
+                             
+                          #   )
+    
         #Microphone Button
-        self.mic_button = Button(text= "Mic", size_hint = (None,None), size = (100,50), background_color = "#B700FF"
-                                )
+        #self.mic_button = Button(text= "Mic", size_hint = (None,None), size = (100,50), background_color = "#B700FF"
+                               # )
 
-        self.button.bind(on_press= self.callback)
+       # self.button.bind(on_press= self.callback)
 
 
 
-        self.window.add_widget(self.button)
+       # self.window.add_widget(self.button)
 
         #Horizontal Layout for Buttons
-        horizontal_layout = BoxLayout(orientation='horizontal', spacing=20, padding=20)
+        #horizontal_layout = BoxLayout(orientation='horizontal', spacing=20, padding=20)
 
-        horizontal_layout.add_widget(self.mic_button)
+       # horizontal_layout.add_widget(self.mic_button)
         #horizontal_layout.add_widget(self.button)
 
-        self.window.add_widget(horizontal_layout)
+        #self.window.add_widget(horizontal_layout)
 
         return self.window
 
-    def callback(self, event):
-        self.greeting.text = "Loading ..."
-        threading.Thread(target=self.long).start()
+    #def callback(self, event):
+       # self.greeting.text = "Loading ..."
+       # threading.Thread(target=self.long).start()
 
-    def long(self):
-        time.sleep(.5)  #only simulating a task, replace with function later
+    #def long(self):
+       # time.sleep(.5)  #only simulating a task, replace with function later
 
-        Clock.schedule_once(self.update_greeting)
+       # Clock.schedule_once(self.update_greeting)
 
-    def update_greeting(self, event):
-        self.greeting.text = "Hello " + self.user.text
+   # def update_greeting(self, event):
+       # self.greeting.text = "Hello " + self.user.text
 
 
    
